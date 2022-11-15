f35589a3037ba464c8c4d7d3db903eea4e3ac2bc
Jonathan Yakimow <jdyakimowusdi@outlook.com>
2022-11-15 00:03:51 -0600

    updates

--- usdi_fmstr/main.py
+++ usdi_fmstr/main.py
@@ -1,10 +1,13 @@
 #usdi freemaster
 
 #library imports
+#install pyserial using instructions at https://pyserial.readthedocs.io/en/latest/pyserial.html
 import serial
 
+#import other files in project
+
 def main():
-    print("hello world")
+    print("Hello World")
 
 if __name__ == "__main__":
     main()
