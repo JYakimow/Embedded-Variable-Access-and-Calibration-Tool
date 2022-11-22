784564597fbd5cde112d9f52fe805b8d2ecbca19
Jonathan Yakimow <jdyakimowusdi@outlook.com>
2022-11-22 03:00:34 -0600

    updates

--- usdi_fmstr/main.py
+++ usdi_fmstr/main.py
@@ -1,13 +1,65 @@
-#usdi freemaster
+"""
+*******************************************************************************
+      File Name  : main.py
+      Author     : jdyakimow
+      Date       : 11/18/2022
+      Description: main file for usdi freemaster pc application
+ ******************************************************************************
+"""
+
+"""
+ ******************************************************************************
+ * IMPORTS
+ ******************************************************************************
+"""
 
 #library imports
-#install pyserial using instructions at https://pyserial.readthedocs.io/en/latest/pyserial.html
-import serial
 
-#import other files in project
+#file imports
+from pickle import TRUE
+import uart
+
+"""
+ ******************************************************************************
+ * VARIABLES and CONSTANTS
+ ******************************************************************************
+"""
+
+"""
+ ******************************************************************************
+ * FUNCTIONS
+ ******************************************************************************
+"""
 
 def main():
-    print("Hello World")
+    print("Application Launched\n")
+
+    uart.uartInit()
+
+    #print(uart.receiveByte())
+
+    #print(uart.receiveByte())
+
+    #print(uart.ser.read())
+    #uart.ser.open()
+    dat = uart.receiveBytes(12)
+    print(dat)
+    """msg = input("Enter something to send to microcontroller: ")
+    uart.ser.write(input(msg.encode()))
+    newdat = uart.ser.read(50);
+    print(newdat)"""
+    
+    x = True
+    while (x == True):
+        msg = input("Enter something to send to microcontroller: ")
+        msgLength = len(msg)
+        #print("length of message: ", msgLength, "\n")
+        uart.ser.write(b'test')
+        print("1")
+        newdat = uart.ser.read(4) #.decode('ascii')
+        print("2")
+        print(newdat)
+        print("end")
 
 if __name__ == "__main__":
     main()
---
+++ usdi_fmstr/uart.py
@@ -0,0 +1,85 @@
+"""
+*******************************************************************************
+      File Name  : uart.py
+      Author     : jyakimow
+      Date       : 11/18/2022
+      Description: uart code for usdi freemaster pc application
+ ******************************************************************************
+"""
+
+"""
+ ******************************************************************************
+ * IMPORTS
+ ******************************************************************************
+"""
+
+#library imports
+#from ast import Constant
+import serial
+
+#file imports
+
+"""
+ ******************************************************************************
+ * VARIABLES and CONSTANTS
+ ******************************************************************************
+"""
+
+#communication commands
+COMMUNICATION_CHECK = 0x55
+COMMUNICATION_ACK = 0x56
+CHANGE_VARIABLE = 0x51
+READ_VARIABLE = 0x52
+
+
+#variable type commands
+VAR_INT = 0x11
+VAR_CHAR = 0x12
+VAR_STRING = 0x13
+VAR_BOOL = 0x14
+
+
+#define serial port
+ser = serial.Serial() #this must set to com port being used
+#ser.close()
+
+"""
+ ******************************************************************************
+ * FUNCTIONS
+ ******************************************************************************
+"""
+
+"""
+ * Function: 		uartInit()
+ * Description: 	initialize uart serial port settings
+ * Parameters:		None
+ * Return Value:	None
+"""
+def uartInit():
+    ser.baudrate = 115200   #set baudrate
+    ser.port = 'COM3'       #set com port
+    #ser.timeout = 1        #timeout after one read
+    ser.open()              #open connection
+
+    #see port info
+    print("COM port opened with following settings: ")
+    print(ser, '\n')
+
+"""
+ * Function: 		sendByte()
+ * Description: 	send byte of data
+ * Parameters:		None
+ * Return Value:	None
+"""
+def sendBytes(length):
+    print("not working yet")
+
+"""
+ * Function: 		receiveByte()
+ * Description: 	send byte of data
+ * Parameters:		None
+ * Return Value:	None
+"""
+def receiveBytes(length):
+    data = ser.read(length).decode('ascii')
+    return data
